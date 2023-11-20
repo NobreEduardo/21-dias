172587f27ec7c1998b62737549c775eae436100b
Eduardo Nobre <duhnobre@gmail.com>
2023-11-20 12:21:35 -0300

    Att aula 4

---
+++ Dia 4/Desafio/Desafio4.js
@@ -0,0 +1,13 @@
+//criando váriaveis com as informações fornecidas pelo usuario
+
+const fome = prompt("Você está com fome? (Sim/Não)");
+const dinheiro = prompt("Você tem dinheiro? (Sim/Não)");
+const restaurante = prompt("O restaurante está aberto?  (Sim/Não)")
+
+if(fome == "Não" || dinheiro == "Não"){
+    console.log ("Hoje a janta será em casa")
+}else if(fome == "Sim" && dinheiro == "Sim" && restaurante == "Não"){
+    console.log ("Peça na Nativa")
+} if (fome == "Sim" && dinheiro == "Sim" && restaurante == "Sim"){
+    console.log("Hoje o jantar vai ser PF")
+}

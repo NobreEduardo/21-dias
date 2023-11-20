d8379d2900906bf1e8da91e6b6f9362ce777e6cd
Eduardo Nobre <duhnobre@gmail.com>
2023-11-20 12:25:17 -0300

    Att no arquivo.js com mais informações

--- Dia 4/Desafio/Desafio4.js
+++ Dia 4/Desafio/Desafio4.js
@@ -4,6 +4,9 @@ const fome = prompt("Você está com fome? (Sim/Não)");
 const dinheiro = prompt("Você tem dinheiro? (Sim/Não)");
 const restaurante = prompt("O restaurante está aberto?  (Sim/Não)")
 
+
+
+// if e else com 3 cenários utilizando as váriaveis criadas anteriormente.
 if(fome == "Não" || dinheiro == "Não"){
     console.log ("Hoje a janta será em casa")
 }else if(fome == "Sim" && dinheiro == "Sim" && restaurante == "Não"){
